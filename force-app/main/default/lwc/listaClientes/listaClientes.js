74ecd9bafa8d5c83d64c538a92600b1954364ad7
Daniel Tabosa <danieltabosa28@gmail.com>
2026-07-12 19:04:16 -0300

    feat: add Apex create method for Cliente__c

--- force-app/main/default/lwc/listaClientes/listaClientes.js
+++ force-app/main/default/lwc/listaClientes/listaClientes.js
@@ -1,9 +1,24 @@
 import { LightningElement, wire } from 'lwc';
 import buscarClientes from '@salesforce/apex/ClienteController.buscarClientes';
 
+
 export default class ListaClientes extends LightningElement {
 
     titulo = 'SalesSync CRM';
+
+    nome = '';
+    empresa = '';
+    email = '';
+    status = '';
+
+   capturarCampo(event) {
+
+    console.log('Campo:', event.target.dataset.campo);
+    console.log('Valor:', event.target.value);
+
+    }
+
+
     @wire(buscarClientes) 
     clientes;
 
